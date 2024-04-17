ca4cfcede43346ea9645a84922c04c420ae46e02
exweas <161201802+exweas@users.noreply.github.com>
2024-04-17 19:53:47 +0800

    Update cipher test.py

--- cipher test.py
+++ cipher test.py
@@ -4,4 +4,15 @@ def vigenere(text, key, mode):
     key_length = len(key)  # определение длины ключа
     encrypted_text = ''  # инициализация зашифрованного текста
 
-
+    for i in range(len(text)):  # проход по символам текста
+        if text[i].isalpha():  # если символ буквенный
+            key_char = key[i % key_length]  # выбор символа ключа для данной итерации
+            if mode == 'encrypt':  # если режим зашифровки
+                shift = alphabet.index(key_char)
+            elif mode == 'decrypt':  # если режим расшифровки
+                shift = -alphabet.index(key_char)  # определение сдвига для расшифровки
+            encrypted_char = alphabet[(alphabet.index(text[i].upper()) + shift) % 26]  # шифрование символа
+            encrypted_text += encrypted_char if text[i].isupper() else encrypted_char.lower()  # добавление символа к зашифрованному тексту, учитывая регистр
+        else:
+            encrypted_text += text[i]  # добавление символа к зашифрованному тексту без шифровки
+            return encrypted_text
