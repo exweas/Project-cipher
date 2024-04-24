1556e31f474716dba207f1bf7d52df79ee58a5fa
exweas <161201802+exweas@users.noreply.github.com>
2024-04-24 23:02:12 +0800

    111

--- cipher test.py
+++ cipher test.py
@@ -2,7 +2,7 @@ def vigenere(text, key, mode):
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # определение алфавита
     key = key.upper()  # перевод ключа в верхний регистр
     key_length = len(key)  # определение длины ключа
-    encrypted_text = ''  # инициализация зашифрованного текста
+    result_text = ''  # инициализация  текста с результатом
 
     for i in range(len(text)):  # проход по символам текста
         if text[i].isalpha():  # если символ буквенный
@@ -11,8 +11,18 @@ def vigenere(text, key, mode):
                 shift = alphabet.index(key_char)
             elif mode == 'decrypt':  # если режим расшифровки
                 shift = -alphabet.index(key_char)  # определение сдвига для расшифровки
-            encrypted_char = alphabet[(alphabet.index(text[i].upper()) + shift) % 26]  # шифрование символа
-            encrypted_text += encrypted_char if text[i].isupper() else encrypted_char.lower()  # добавление символа к зашифрованному тексту, учитывая регистр
+            result_char = alphabet[(alphabet.index(text[i].upper()) + shift) % 26]  # шифрование/расшифровывание символа
+            result_text += result_char if text[i].isupper() else result_char.lower()  # добавление символа к результату, учитывая регистр
         else:
-            encrypted_text += text[i]  # добавление символа к зашифрованному тексту без шифровки
-            return encrypted_text
+            result_text += text[i]  # добавление символа к результату без шифровки
+    return result_text
+
+text = 'What is love'  # исходный текст
+key = 'Monkey'  # ключ
+encrypted_text = vigenere(text, key, 'encrypt')  # зашифрованный текст
+decrypted_text = vigenere(encrypted_text, key, 'decrypt')  # расшифрованный текст
+
+print('Original text:', text)  # вывод исходного текста
+print('Encrypted text:', encrypted_text)  # вывод зашифрованного текста
+print('Decrypted text:', decrypted_text)  # вывод расшифрованного текста
+
